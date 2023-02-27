ffad2aaf037326dd6750819ccf28133dd69bdb98
manojdaniel94 <manoj.daniel@ltts.com>
2023-02-27 11:33:36 +0530

    fix: new base created

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import Router from "./routes/Router";
+import "./styles.scss";
+import BreadCrumb from "./components/BreadCrumb/BreadCrumb";
+import CalendarComponent from "./components/CalendarComponent/CalendarComponent";
 
-function App() {
+const App = () => {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div id="container">
+        <Header />
+        <div id="content">
+          <div className="content-inner">
+            <div id="breadcrumb">
+              <BreadCrumb />
+              <CalendarComponent />
+            </div>
+            <Router />
+          </div>
+        </div>
+        <Footer />
+      </div>
+
+    </>
   );
-}
+};
 
 export default App;
